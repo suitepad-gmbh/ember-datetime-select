34d8493fec3e6d29952f0388a4eb337f539dde61
Paul Spieker <p.spieker@duenos.de>
2016-03-24 18:32:11 +0100

    fix: null exceptions

--- tests/unit/helpers/date-eq-test.js
+++ tests/unit/helpers/date-eq-test.js
@@ -8,8 +8,28 @@ import {
   dateEq
 } from 'ember-datetime-select/helpers/date-eq';
 
+const date1 = new Date(Date.parse('2016-01-01T15:00:00Z'));
+const date2 = new Date(Date.parse('2016-01-01T18:00:00Z'));
+const date3 = new Date(Date.parse('2016-01-07T18:00:00Z'));
+
 describe('DateEqHelper', function() {
   it('is a function', function() {
     expect(dateEq).to.be.a('function');
+  });
+
+  it('is false if one param is not a date', function() {
+    expect(dateEq([new Date(), null])).to.be.false;
+  });
+
+  it('is false if both params is not a date', function() {
+    expect(dateEq([null, null])).to.be.false;
+  });
+
+  it('is true if both params are the same date', function() {
+    expect(dateEq([date1, date2])).to.be.true;
+  });
+
+  it('is true if both params are different dates', function() {
+    expect(dateEq([date2, date3])).to.be.false;
   });
 });
--- tests/unit/helpers/time-eq-test.js
+++ tests/unit/helpers/time-eq-test.js
@@ -4,13 +4,30 @@ import {
   describe,
   it
 } from 'mocha';
-import {
-  timeEq
-} from 'ember-datetime-select/helpers/time-eq';
+import { timeEq } from 'ember-datetime-select/helpers/time-eq';
+
+const time1 = new Date(Date.parse('2016-01-01T15:00:00Z'));
+const time2 = new Date(Date.parse('2016-01-01T18:00:00Z'));
+const time3 = new Date(Date.parse('2016-01-07T18:00:00Z'));
 
 describe('TimeEqHelper', function() {
-  // Replace this with your real tests.
   it('is a function', function() {
     expect(timeEq).to.be.a('function');
+  });
+
+  it('is false if one param is not a time', function() {
+    expect(timeEq([new Date(), null])).to.be.false;
+  });
+
+  it('is false if both params is not a time', function() {
+    expect(timeEq([null, null])).to.be.false;
+  });
+
+  it('is true if both params are the same time', function() {
+    expect(timeEq([time2, time3])).to.be.true;
+  });
+
+  it('is true if both params are different times', function() {
+    expect(timeEq([time1, time3])).to.be.false;
   });
 });
